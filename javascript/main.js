ead7ee76c3c9f44892828d58cebad4a040d15d85
Mottaqui Karim <taqqui.karim@gmail.com>
2017-08-15 01:08:09 -0400

    v1 of practice problem

---
+++ javascript/main.js
@@ -0,0 +1,59 @@
+/* CURRENTLY IN: javascript/main.js */
+
+/*
+	YOU ARE TO CREATE A SIMPLE CASH REGISTER
+
+	THE USER INPUTS A VALUE (IE A NUMBER)
+	AND YOUR "TOTAL" HTML ELEMENT MUST REFLECT THIS
+
+
+	PHASE 1:
+		* w/partner: SIMPLY __COMMENT__ out all these lines of code 
+		  so that you are clear as to what is happening. feel free to console.log
+		  as you see fit
+	
+	PHASE 2:
+		* this register is clearly broken. fix it so that the `total` value
+		is properly reflected as user adds more numbers (HINT: you will
+		need to alter a function to pull this off properly)
+		* using only the functions provided, update the totalSpan html
+		to reflect the new total as user adds numbers
+		* your code should *ALSO* clear the input field when the user hits
+		the add button
+
+	PHASE 3:
+		* sanitize your input. what if the user adds gibberish or text instead of
+		numbers? account for these possibilities
+		* what if the user adds a '$' before or after the number? account for these
+		possibilities
+
+	PHASE 4:
+		* as a user, I want to hit the enter key to be able to carry out the 
+		same effect that the 'add' button carries. implement logic to achieve this
+*/
+
+const setValue = ($target, value) => {
+	if ($target === null) {
+		return false;
+	}
+
+	$target.innerHTML = value;
+	return true;
+}
+const getValueFromInput = ($inp) => {
+	return $inp.value;
+}
+
+const addNum = (a,b) => {
+	return a+b;
+}
+
+let total = 0;
+const $totalSpan = document.querySelector('.js-total');
+const $add = document.querySelector('.js-add');
+const $input = document.querySelector('.js-input');
+
+$add.addEventListener('click', (e) => {
+	const input = getValueFromInput($input);
+
+});
